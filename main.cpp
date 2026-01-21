d0c712db8da4062da9556845cd421b11091612be
xqill275 <oliverlong275@gmail.com>
2026-01-21 12:32:41 +0000

    w

--- main.cpp
+++ main.cpp
@@ -83,7 +83,7 @@ int main() {
 
 
     // Load ROM
-    cartridge cart("C:/Users/olive/CLionProjects/untitled1/roms/nestest.nes");
+    cartridge cart("C:/Users/olive/CLionProjects/untitled1/roms/donkeykong.nes");
     bus.insertCartridge(&cart);
     bus.reset();
 
